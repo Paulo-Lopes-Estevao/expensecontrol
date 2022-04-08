ec64492a17d6bacd902ab1ff51c671f0c5793628
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-04-08 19:29:51 +0100

    fit: corrugi dialects:postgres

--- config/database.py
+++ config/database.py
@@ -6,7 +6,7 @@ import psycopg2
 from config.env import *
 
 
-SQLALCHEMY_DATABASE_URL = os.environ['DATABASE_URL']
+SQLALCHEMY_DATABASE_URL = os.environ['DATABASE_URL'].replace("://", "ql://", 1)
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
